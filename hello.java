db912983234c74435476c0f246930d18d3f39775
Kavya <159226897+jyothinavya23@users.noreply.github.com>
2024-05-12 12:13:53 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -3,7 +3,7 @@ public class hell java
 	public static void main(String[]args)
 	{
 		System.out.println("java language");
-		System.out.println("oop");
+		System.out.println("oop object oriented language");
 		System.out.println("welcome to spt");
 	}
 }
